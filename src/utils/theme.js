e26b1c278f2d0dcf79651dfc8fef062f38b77611
cnsvilca <carlosvilca.juy@gmail.com>
2023-08-25 20:41:03 -0300

    add: utiles COLORS, SPACING, FONT_SIZE

---
+++ src/utils/theme.js
@@ -0,0 +1,27 @@
+export const COLORS = {
+    primary: '#643843',
+    secondaty: '#99627A',
+    text: '#E7CBCB',
+    inactive: '#C88EA7',
+    white: '#FFFFFF',
+    grey: '#F1F1F1'
+}
+export const SPACING = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 40,
+    xxl: 48,
+    xxxl: 64
+}
+
+export const FONT_SIZE = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 40,
+    xxl: 48,
+    xxxl: 64
+  }
