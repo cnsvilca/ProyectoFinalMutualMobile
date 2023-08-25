a05cc73458f659865d31700cff76dce6d6cd160e
cnsvilca <carlosvilca.juy@gmail.com>
2023-08-25 20:55:15 -0300

    add: archivo data con datos cuotas de prueba

---
+++ src/api/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: '1',
+        fecha_vencimiento: '09-02-22',
+        monto: 10500,
+        estado_pagado: true,
+        orden_id: 1
+    },
+    {
+        id: '2',
+        fecha_vencimiento: '15-12-21',
+        monto: 4000,
+        estado_pagado: true,
+        orden_id: 5
+    },
+    {
+        id: '3',
+        fecha_vencimiento: '18-04-19',
+        monto: 2000,
+        estado_pagado: true,
+        orden_id: 4
+    },
+    {
+        id: '4',
+        fecha_vencimiento: '13-10-24',
+        monto: 1100,
+        estado_pagado: true,
+        orden_id: 3
+    },
+    {
+        id: '5',
+        fecha_vencimiento: '20-09-17',
+        monto: 4500,
+        estado_pagado: true,
+        orden_id: 2
+      },
+]
