7ebf8f7a5f4ba8af4954a89e2c9df14a5ced21c3
cnsvilca <carlosvilca.juy@gmail.com>
2023-08-25 21:55:37 -0300

    feat: pantalla de resumen de afiliado

--- src/screens/user/UserInfoScreen.jsx
+++ src/screens/user/UserInfoScreen.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { UserContext } from '../../contexts/UserContext'
 
 export const UserInfoScreen = () => {
-  return (
-    <View>
-      <Text>User Info Screen</Text>
-    </View>
-  )
+    const { currentUser, setCurrentUser } = useContext(UserContext)
+
+    const handleLogout = () => {
+        setCurrentUser(null)
+    }
+
+    return (
+        <View >
+            <View >
+                <View >
+                    <Text>Nombre y Apellido</Text>
+                    <Text>
+                        {currentUser.afiliado.name} {currentUser.afiliado.lastname}
+                    </Text>
+                </View>
+                <View>
+                    <Text>Saldo Disponible</Text>
+                    <Text>$ {currentUser.afiliado.saldo}</Text>
+                </View>
+            </View>
+            <View>
+                <TouchableOpacity  onPress={handleLogout}>
+                    <Text>Salir</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    )
 }
