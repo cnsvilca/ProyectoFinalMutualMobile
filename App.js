57827c05ec86a25231f76a97a357394ae34b136a
cnsvilca <carlosvilca.juy@gmail.com>
2023-08-25 21:23:11 -0300

    feat: funcionalidad bottom-tabs para navegacion

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ProfileScreen } from './src/screens/profile/ProfileScreen';
+import { PaymentListScreen } from './src/screens/payment-list/PaymentListScreen';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { COLORS, SPACING } from './src/utils/theme'
+import { UserProvider } from './src/contexts/UserContext';
+
+const Tab = createBottomTabNavigator()
+
+const TAB_ICON = {
+  Profile: 'face-man-profile',
+  Payment: 'calendar-clock'
+}
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name]
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <MaterialCommunityIcons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: COLORS.primary,
+    tabBarInactiveTintColor: COLORS.inactive,
+    headerShown: false,
+    tabBarStyle: styles.tabBar
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <UserProvider>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={screenOptions}>
+            <Tab.Screen name='Profile' options={{ title: 'Inicio' }} component={ProfileScreen}></Tab.Screen>
+            <Tab.Screen name='Payment' options={{ title: 'Cuotas' }} component={PaymentListScreen}></Tab.Screen>
+          </Tab.Navigator>
+        </NavigationContainer>
+        <StatusBar style='auto'></StatusBar>
+      </UserProvider>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  tabBar: {
+    height: SPACING.xxxl,
+    paddingBottom: SPACING.xs,
+    paddingTop: SPACING.xs
+  }
+})
---
+++ src/screens/payment-list/PaymentListScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const PaymentListScreen = () => {
+  return (
+    <View>
+      <Text>Cuotas Screen</Text>
+    </View>
+  )
+}
